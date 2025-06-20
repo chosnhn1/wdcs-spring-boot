6349a1b1daedf255a670cbed90aa67c47f00420a
Cho Seonghan <chosnhn1@gmail.com>
2025-06-20 17:22:52 +0900

    add Answer tests, refactor Question and Answer packages, add new Controller

--- src/test/groovy/com/mysite/sbb/SbbApplicationTests.groovy
+++ src/test/groovy/com/mysite/sbb/SbbApplicationTests.groovy
@@ -1,11 +1,13 @@
 package com.mysite.sbb
 
-//import java.time.LocalDateTime;
-import java.util.List;
+import com.mysite.sbb.answer.Answer
+import com.mysite.sbb.answer.AnswerRepository
+import com.mysite.sbb.question.Question
+import com.mysite.sbb.question.QuestionRepository
+import org.springframework.transaction.annotation.Transactional
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertTrue
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,24 +19,76 @@ class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 
+	@Autowired
+	private AnswerRepository answerRepository;
+
+	@Transactional
 	@Test
 	void testJpa() {
 
-		List<Question> qList = this.questionRepository.findBySubjectLike("%sbb%");
-		Question q = qList.get(0);
-		assertEquals("What is sbb?", q.getSubject());
+//		Test #11: get answers from a question
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
 
-//		Test #5
+//		the code below will get a failed test, because it had no session
+//		to do this test, @Transactional annotation will be required.
+		List<Answer> answerList = q.getAnswerList();
+		assertEquals(1, answerList.size());
+		assertEquals("Yes, it is created automatically.", answerList.get(0).getContent());
+
+//		Test #10: find answer with id
+//		Optional<Answer> oa = this.answerRepository.findById(1);
+//		assertTrue(oa.isPresent());
+//		Answer a = oa.get();
+//		assertEquals(2, a.getQuestion().getId());
+
+//		Test #9: create an answer
+//		Optional<Question> oq = this.questionRepository.findById(2);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//
+//		Answer a = new Answer();
+//		a.setContent("Yes, it is created automatically.");
+//		a.setQuestion(q);
+//		a.setCreateDate(LocalDateTime.now());
+//		this.answerRepository.save(a);
+
+
+//		Test #8: delete question
+
+//		assertEquals(2, this.questionRepository.count());			// count method of Repository returns data row count
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		this.questionRepository.delete(q);
+//		assertEquals(1, this.questionRepository.count());			// check one of two is deleted
+
+//		Test #7: modify question
+
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		q.setSubject("modified title");
+//		this.questionRepository.save(q);
+
+//		Test #6: LIKE search with subject
+
+//		List<Question> qList = this.questionRepository.findBySubjectLike("%sbb%");
+//		Question q = qList.get(0);
+//		assertEquals("What is sbb?", q.getSubject());
+
+//		Test #5: find with both subject and content
 
 //		Question q = this.questionRepository.findBySubjectAndContent("What is sbb?", "Please give me some answer!");
 //		assertEquals(1, q.getId())
 
-//		Test #4
+//		Test #4: find with subject
 
 //		Question q = this.questionRepository.findBySubject("What is sbb?");
 //		assertEquals(1, q.getId());
 
-//		Test #3: search q by id and check subject
+//		Test #3: find q by id and check subject
 
 //		Optional<Question> oq = this.questionRepository.findById(1);
 //		if(oq.isPresent()) {

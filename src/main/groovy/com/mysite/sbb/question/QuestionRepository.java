3f28422aed42614871b5c87ecb0751739307ed14
Cho Seonghan <chosnhn1@gmail.com>
2025-07-11 15:49:06 +0900

    add findAllByKeyword into QuestionRepository: it will work with sql query in search

--- src/main/groovy/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/groovy/com/mysite/sbb/question/QuestionRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer>{
 
@@ -19,6 +21,25 @@ public interface QuestionRepository extends JpaRepository<Question, Integer>{
 //    for pagination
 //    note: Spring Pagination starts with 0
     Page<Question> findAll(Pageable pageable);
+
+//    for search
     Page<Question> findAll(Specification<Question> spec, Pageable pageable);
+
+//    if you can write SQL query, this will work
+//    note: use Entity name not table name, also do like q.author=u1 in column not q.author_id=u1.id
+//    pass keyword with @Param and ref it with :keyword
+    @Query("SELECT "
+            + "DISTINCT q "
+            + "FROM Question q "
+            + "LEFT OUTER JOIN SiteUser u1 ON q.author=u1 "
+            + "LEFT OUTER JOIN Answer a ON a.question=q "
+            + "LEFT OUTER JOIN SiteUser u2 ON a.author=u2 "
+            + "WHERE "
+            + "q.subject LIKE %:kw% "
+            + "OR q.content LIKE %:kw% "
+            + "OR u1.username LIKE %:kw% "
+            + "OR a.content LIKE %:kw% "
+            + "OR u2.username LIKE %:kw% ")
+    Page<Question> findAllByKeyword(@Param("kw") String kw, Pageable pageable);
 
 }

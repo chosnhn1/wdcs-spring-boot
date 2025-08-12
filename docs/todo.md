640fed5097c55a727414577c692c1075d64edd10
Cho Seonghan <chosnhn1@gmail.com>
2025-08-12 16:04:58 +0900

    edit on docs; ditch unused imports on SecurityConfig

--- docs/todo.md
+++ docs/todo.md
@@ -1,5 +1,5 @@
 # What can be done more to this project?
 
-* Switching DB to proper service (ie. Postgresql)
+* Switching DB to proper service (i.e. Postgresql)
 * change res > req structure to conform to restful
-
+* Ditch depreciated methods (`AntPathRequestMatcher`, etc.) - Done
--- src/main/groovy/com/mysite/sbb/SecurityConfig.java
+++ src/main/groovy/com/mysite/sbb/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.mysite.sbb;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,17 +7,10 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.web.PathPatternRequestMatcherBuilderFactoryBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
-import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.OrRequestMatcher;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.web.util.pattern.PathPatternParser;
 
 // Set Spring Security Configurations here:
 
